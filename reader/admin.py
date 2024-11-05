838710887081e0641d1bbb51931ab588d47eed8a
himaldahal <51692864+himaldahal@users.noreply.github.com>
2024-11-05 22:27:16 +0545

    finalized code with working prototype

--- reader/admin.py
+++ reader/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from reader.models import * 
 
+# This website doesn't require an admin to manage 
 
 admin.site.index_title = 'iLearn management'
 admin.site.site_title = 'iLearn'
 admin.site.site_header = 'iLearn'
 
 
-admin.site.register(Project)
-admin.site.register(Sources)
-admin.site.register(Note)
+# admin.site.register(Project)
+# admin.site.register(Sources)
+# admin.site.register(Note)
